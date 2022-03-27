e4e71a338daf03e4cfea662c5c61e4e15b3f1bc6
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-03-27 20:51:16 +0700

    Update FirmwareNongpuserver.sh

--- FirmwareNongpuserver.sh
+++ FirmwareNongpuserver.sh
@@ -137,6 +137,7 @@ sudo pip3 install pyglet
 sudo pip3 install PyWave 
 sudo pip3 install pyvista 
 sudo pip3 install plotly
+sudo pip3 install dash 
 sudo pip3 install pyaudio 
 sudo pip3 install googletrans==4.0.0rc1  # Google translate 
 sudo pip3 install google-speech # Google Speech function
