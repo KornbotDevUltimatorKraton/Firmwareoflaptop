5167d085db8d17381016f56775a370bd9d12d169
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-07-28 12:17:53 +0700

    Update FirmwareNongpuserver.sh

--- FirmwareNongpuserver.sh
+++ FirmwareNongpuserver.sh
@@ -90,6 +90,7 @@ sudo pip3 install protobuf
 sudo pip3 install googlesearch-python
 sudo apt-get install dnsutils -y 
 sudo apt-get install v4l-utils -y 
+sudo apt-get install smstools -y
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 echo"Computer vision installation library install here" 
 sudo apt-get install python3-opencv -y 
