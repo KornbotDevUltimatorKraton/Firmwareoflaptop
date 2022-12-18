ea3efd95a056dbc0547e12d13732314b66b1990d
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-12-18 12:02:50 +0700

    Update FirmwareNongpuserver.sh

--- FirmwareNongpuserver.sh
+++ FirmwareNongpuserver.sh
@@ -360,6 +360,7 @@ npm install express
 sudo pip3 install flask==2.1.1
 sudo pip3 install flask_wtf
 sudo pip3 install mysql-connector-python
+sudo apt-get install gnome-tweaks -y 
 #sudo pip3 install mysql-connector-python
 #sudo pip3 install werkzeug==0.16.1
 #sudo apt-get update 
