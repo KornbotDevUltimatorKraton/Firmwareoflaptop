f7348fefbb0fcb8f82263b45441b0ba7b01e560a
Chanapai Chuadchum <kornbot359@hotmail.com>
2023-09-11 13:45:47 +0700

    Update FirmwareNongpuserver.sh

--- FirmwareNongpuserver.sh
+++ FirmwareNongpuserver.sh
@@ -232,6 +232,7 @@ sudo apt-get install pyqt5-dev-tools -y
 sudo pip3 install wonderwords
 sudo apt-get install qttools5-dev-tools -y 
 sudo pip3 install pyqtgraph 
+sudo apt install libpoppler-qt5-dev -y 
 sudo apt-get install --upgrade python3-pyside2.qt3dcore python3-pyside2.qt3dinput python3-pyside2.qt3dlogic python3-pyside2.qt3drender python3-pyside2.qtcharts python3-pyside2.qtconcurrent python3-pyside2.qtcore python3-pyside2.qtgui python3-pyside2.qthelp python3-pyside2.qtlocation python3-pyside2.qtmultimedia python3-pyside2.qtmultimediawidgets python3-pyside2.qtnetwork python3-pyside2.qtopengl python3-pyside2.qtpositioning python3-pyside2.qtprintsupport python3-pyside2.qtqml python3-pyside2.qtquick python3-pyside2.qtquickwidgets python3-pyside2.qtscript python3-pyside2.qtscripttools python3-pyside2.qtsensors python3-pyside2.qtsql python3-pyside2.qtsvg python3-pyside2.qttest python3-pyside2.qttexttospeech python3-pyside2.qtuitools python3-pyside2.qtwebchannel python3-pyside2.qtwebsockets python3-pyside2.qtwidgets python3-pyside2.qtx11extras python3-pyside2.qtxml python3-pyside2.qtxmlpatterns -y 
 sudo pip3 install pymatgen 
 sudo pip3 install matminer 
